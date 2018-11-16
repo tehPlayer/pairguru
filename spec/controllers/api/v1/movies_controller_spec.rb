67c5eefd907844f800ff67da53cf9284f2333761
Marcin Adamczyk <adamczyk.marcin@outlook.com>
2018-11-17 00:45:32 +0100

    fix expectations in v1/ and v2/movies#index specs

--- spec/controllers/api/v1/movies_controller_spec.rb
+++ spec/controllers/api/v1/movies_controller_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 describe Api::V1::MoviesController, type: :controller do
   describe "#index" do
-    let(:movies) { create_list(:movie, 5) }
+    let!(:movies) { create_list(:movie, 5) }
 
-    let(:response) do
+    let(:movies_response) do
       movies.map do |movie|
-        {id: movie.id, title: movie.title}
+        {"id" => movie.id, "title" => movie.title}
       end
     end
 
     it "returns an array of movies with id and title only" do
       get :index
-      expect(response).to eq(response)
+      expect(JSON.parse(response.body)).to eq(movies_response)
     end
   end
 end
--- spec/controllers/api/v2/movies_controller_spec.rb
+++ spec/controllers/api/v2/movies_controller_spec.rb
@@ -4,16 +4,16 @@ describe Api::V2::MoviesController, type: :controller do
   describe "#index" do
     let(:movies) { create_list(:movie, 5) }
 
-    let(:response) do
+    let(:expected_response) do
       movies.map do |movie|
         genre = movie.genre
         {
-          id: movie.id,
-          title: movie.title,
-          genre: {
-            id: genre.id,
-            name: genre.name,
-            movies_count: genre.movies.count
+          "id" => movie.id,
+          "title" => movie.title,
+          "genre" => {
+            "id" => genre.id,
+            "name" => genre.name,
+            "movies_count" => genre.movies.count
           }
         }
       end
@@ -21,7 +21,7 @@ describe Api::V2::MoviesController, type: :controller do
 
     it "returns an array of movies with id, title and genre object" do
       get :index
-      expect(response).to eq(response)
+      expect(response).to eq(expected_response)
     end
   end
 end
