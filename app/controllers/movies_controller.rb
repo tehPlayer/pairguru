4f983baeffb9fc3c5e22358c919a56e4d031d211
Marcin Adamczyk <adamczyk.marcin@outlook.com>
2018-11-18 00:52:52 +0100

    change movies controller to use async delivery

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
 
   def send_info
     @movie = Movie.find(params[:id])
-    MovieInfoMailer.send_info(current_user, @movie).deliver_now
+    MovieInfoSendWorker.perform_async(current_user.id, @movie.id)
     redirect_back(fallback_location: root_path, notice: "Email sent with movie info")
   end
 
