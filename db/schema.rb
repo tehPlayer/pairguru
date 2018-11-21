d9e16153fbc059844eb17456390ec5647cf360bc
Marcin Adamczyk <adamczyk.marcin@outlook.com>
2018-11-21 19:11:55 +0100

    add comment views (form and partial for list), add comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_235926) do
+ActiveRecord::Schema.define(version: 2018_11_21_150231) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
